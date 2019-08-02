e425dd24a69798b146d6c0b3e3aad192d4a61e68
Suresh kumar Dasagranthishanmugam <suresh@rbchealthcare.com>
2019-08-01 22:16:32 -0500

    - New

---
+++ android/lib/src/main/java/com/swmansion/gesturehandler/ViewConfigurationHelperImpl.java
@@ -0,0 +1,22 @@
+package com.swmansion.gesturehandler;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ViewConfigurationHelperImpl implements ViewConfigurationHelper {
+
+  @Override
+  public PointerEventsConfig getPointerEventsConfigForView(View view) {
+    return view.isEnabled() ? PointerEventsConfig.AUTO : PointerEventsConfig.NONE;
+  }
+
+  @Override
+  public View getChildInDrawingOrderAtIndex(ViewGroup parent, int index) {
+    return parent.getChildAt(index);
+  }
+
+  @Override
+  public boolean isViewClippingChildren(ViewGroup view) {
+    return false;
+  }
+}
